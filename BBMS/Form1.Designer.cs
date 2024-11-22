baf583d2144bc0cd1c8b6a61f8492556e6c09359
Dipa Tamang <tamangdipa77@gmail.com>
2024-11-22 12:11:07 +0545

    Login successful

--- BBMS/Form1.Designer.cs
+++ BBMS/Form1.Designer.cs
@@ -28,12 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            cbTC = new CheckBox();
+            btnLogin = new Button();
+            linkLabel1 = new LinkLabel();
+            linkLabel2 = new LinkLabel();
+            linkLabel3 = new LinkLabel();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.LightGray;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Segoe Script", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.ForeColor = Color.Maroon;
+            textBox1.Location = new Point(388, 79);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(209, 67);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "Raktdaan";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(375, 132);
+            label1.Name = "label1";
+            label1.Size = new Size(212, 23);
+            label1.TabIndex = 1;
+            label1.Text = "Be a Donor, Be  a Saviour";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(418, 205);
+            label2.Name = "label2";
+            label2.Size = new Size(69, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Hi, there!";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(361, 234);
+            label3.Name = "label3";
+            label3.Size = new Size(236, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Please fill up the following details:";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(401, 294);
+            txtUsername.Name = "txtUsername";
+            txtUsername.PlaceholderText = "Username";
+            txtUsername.Size = new Size(125, 27);
+            txtUsername.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(401, 347);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '*';
+            txtPassword.PlaceholderText = "Password";
+            txtPassword.Size = new Size(125, 27);
+            txtPassword.TabIndex = 5;
+            // 
+            // cbTC
+            // 
+            cbTC.AutoSize = true;
+            cbTC.Location = new Point(327, 406);
+            cbTC.Name = "cbTC";
+            cbTC.Size = new Size(261, 24);
+            cbTC.TabIndex = 6;
+            cbTC.Text = "I agree to all terms and conditions.";
+            cbTC.UseVisualStyleBackColor = true;
+            cbTC.CheckedChanged += cbTC_CheckedChanged;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.LimeGreen;
+            btnLogin.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnLogin.Location = new Point(543, 347);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(54, 27);
+            btnLogin.TabIndex = 7;
+            btnLogin.Text = "->";
+            btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(327, 471);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(123, 20);
+            linkLabel1.TabIndex = 8;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "New Registration";
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.Location = new Point(483, 471);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(125, 20);
+            linkLabel2.TabIndex = 9;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "Forgot Password?";
+            // 
+            // linkLabel3
+            // 
+            linkLabel3.AutoSize = true;
+            linkLabel3.Location = new Point(401, 511);
+            linkLabel3.Name = "linkLabel3";
+            linkLabel3.Size = new Size(127, 20);
+            linkLabel3.TabIndex = 10;
+            linkLabel3.TabStop = true;
+            linkLabel3.Text = "Terms & Conditions";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightGray;
+            ClientSize = new Size(920, 568);
+            Controls.Add(linkLabel3);
+            Controls.Add(linkLabel2);
+            Controls.Add(linkLabel1);
+            Controls.Add(btnLogin);
+            Controls.Add(cbTC);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            WindowState = FormWindowState.Maximized;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private CheckBox cbTC;
+        private Button btnLogin;
+        private LinkLabel linkLabel1;
+        private LinkLabel linkLabel2;
+        private LinkLabel linkLabel3;
     }
 }
--- BBMS/Form1.cs
+++ BBMS/Form1.cs
@@ -6,5 +6,40 @@ namespace BBMS
         {
             InitializeComponent();
         }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "user" && txtPassword.Text == "pass")
+            {
+                MessageBox.Show("Login successful!");
+
+                //to navigate to the Dashboard
+                Dashboard form = new Dashboard();
+                form.Show();
+                Visible = false;
+            }
+            else
+            {
+                if (MessageBox.Show("Incorrect username or password.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning) == DialogResult.OK)
+                {
+                    Application.Exit();
+                }
+
+            }
+
+        }
+
+        private void cbTC_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbTC.Checked == true)
+            {
+                btnLogin.Enabled = true;
+            }
+            else
+            {
+                btnLogin.Enabled = false;
+            }
+
+        }
     }
 }
